2cef6a0a6a79ac13cae087aeb5558acd1c0404a9
Rocco <roccorighi@hotmail.com>
2018-02-15 16:51:01 +0000

    Add initial styling

--- src/App/app.js
+++ src/App/app.js
@@ -7,8 +7,8 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <h1 className="App-title">Crypto-portafoglio</h1>
                 </header>
+                    <h1 className="App-title">Crypto-portafoglio</h1>
                 <AssetContainer />
             </div>
         );
--- src/Asset/asset-presentation.js
+++ src/Asset/asset-presentation.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Label from '../Label/label';
 import Value from '../Value/value';
+import './asset.css';
 
 const AssetPresentation = (props) => {
     const assets = props.assets.map((asset) =>
-        <div key={parseFloat(asset.free)}>
+        <div className="asset" key={parseFloat(asset.free)}>
             <Label assetName={asset.asset} />
             <Value value={asset.free} />
         </div>
     );
     return (
-        <div>
-            {assets};
+        <div className="asset-container">
+            {assets}
         </div>
     )
 }
