180558f94273d568f14d8fcbec4c2b00c6e9a2ea
rocco <rocco.righi@bbc.co.uk>
2018-03-19 18:44:43 +0000

    Add right mockImplementation for coinbase

--- __mocks__/coinbase.js
+++ __mocks__/coinbase.js
@@ -1,4 +1,5 @@
 export const Client = jest.fn(() => ({ getAccounts: getAccountsMock}));
+
 const mock = jest.fn().mockImplementation(() => {
   return { Client };
 });
@@ -6,8 +7,6 @@ const coinbaseResult = [
     { balance: { currency: 'ETH', amount: '0.001' }, native_balance: { amount: '0.1' } }
 ]
 
-const getAccountsMock = jest.fn(({}, ({}, cb) => {
-    cb(false, coinbaseResult)
-}));
+const getAccountsMock = jest.fn(({}, cb) => cb(true, coinbaseResult));
 
 export default mock;
--- src/Server/coinbaseBalance.test.js
+++ src/Server/coinbaseBalance.test.js
@@ -1,25 +1,31 @@
 import getCoinbaseBalance from './coinbaseBalance';
 import { Client } from 'coinbase';
 
-jest.mock('coinbase');
+jest.mock('coinbase', () => {
+    const getAccounts = jest.fn();
+    return { Client: jest.fn(() => ({ getAccounts }))}
+});
 
-const coinbase = Client();
+const coinbase = new Client();
+
+const coinbaseResult = [
+    { balance: { currency: 'ETH', amount: '0.001' }, native_balance: { amount: '0.1' } }
+]
+
 const expectedResult = [{"asset": "ETH", "free": "0.001", "locked": "0.00000000", "price_eur": "0.1"}];
+const rejected = jest.fn();
 
-const rejected = jest.fn().mockImplementation(cb => {
-  cb(true, { history: [1, 2, 3]})
-  })
 test('should return only the owned assets', async () => {
+    coinbase.getAccounts.mockImplementation(({}, cb) => cb(false, coinbaseResult));
     const result = await getCoinbaseBalance();
     expect(result).toEqual(expectedResult);
     expect(coinbase.getAccounts).toHaveBeenCalled();
 });
 
-// test('should return a rejected promise if there is an error', async () => {
-//     coinbase.getAccounts = rejected;
-//     const result = await getCoinbaseBalance();
-//     expect(result).toEqual(expectedResult);
-//     expect(coinbase.getAccounts).toHaveBeenCalled();
-// });
+test('should return a rejected promise if there is an error', async () => {
+    coinbase.getAccounts.mockImplementation(({}, cb) => cb(true));
+    expect(getCoinbaseBalance()).rejects.toEqual(true);
+    expect(coinbase.getAccounts).toHaveBeenCalled();
+});
 
 
