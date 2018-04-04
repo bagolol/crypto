b2cc3f507e2441d57b8f6afe361a8b503ffdc837
rocco <rocco.righi@bbc.co.uk>
2018-04-04 10:19:42 +0100

    Fix build problem

--- src/Server/__mocks__/coinbase.js
+++
@@ -1,7 +0,0 @@
-const coinbaseResult = [
-    {"asset": "ETH", "free": "1.00000000", "locked": "0.00000000", "price_eur": "10"}
-];
-const successfulCoinbasePromise = jest.fn(() => Promise.resolve(coinbaseResult))
-export default successfulCoinbasePromise;
-
-
