b9677f83709cf45da76792f9a7fa760d4d46852b
Rocco <roccorighi@hotmail.com>
2018-04-08 16:40:36 +0100

    Render app isomorphically

--- src/Server/server.js
+++ src/Server/server.js
@@ -29,4 +29,30 @@ app.get('/api/binance', (req, res) => {
         .catch(err => res.send(err));
 });
 
+app.get('*', (req, res) => {
+  let clientJSBundle = 'main.js';
+  let clientCSSBundle = 'main.css';
+  if (process.env.NODE_ENV === 'production') {
+    const manifest = require('optimo-client/build/asset-manifest.json'); // eslint-disable-line global-require,import/no-unresolved
+    clientJSBundle = manifest[clientJSBundle]; // eslint-disable-line
+    clientCSSBundle = manifest[clientCSSBundle]; // eslint-disable-line
+  }
+
+  res.status(200).send(`<!DOCTYPE html>
+                          <html lang="en" class="reith-sans-font reith-serif-font">
+                            <head>
+                              <meta charset="utf-8">
+                              <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                              <title>Optimo</title>
+                              <link rel="stylesheet" href="/${clientCSSBundle}"></script>
+                            </head>
+                            <body>
+                              <noscript>
+                                You need to enable JavaScript to run this app.
+                              </noscript>
+                              <div id="root"></div>
+                              <script src="/${clientJSBundle}"></script>
+                            </body>
+                          </html>`);
+});
 export default app;
