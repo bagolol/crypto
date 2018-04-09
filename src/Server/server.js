8bf8dc264661fc251a43e3b702a80bc3abd2751d
rocco <rocco.righi@bbc.co.uk>
2018-04-09 18:21:21 +0100

    Re-add cors package

--- src/Server/server.js
+++ src/Server/server.js
@@ -2,17 +2,18 @@ import express from 'express';
 import getAssetAndQuotations from './apiCalls';
 import bodyParser from 'body-parser';
 import path from 'path';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use(bodyParser.json());
 
@@ -40,7 +41,7 @@ app.get('*', (req, res) => {
                             <head>
                               <meta charset="utf-8">
                               <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                              <title>Optimo</title>
+                              <title>Crypto-App</title>
                               <link rel="stylesheet" href="/${clientCSSBundle}"></script>
                             </head>
                             <body>
