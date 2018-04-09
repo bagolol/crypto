05873f8ee1f1248202751a2fd2567d9cc38707c2
rocco <rocco.righi@bbc.co.uk>
2018-04-09 18:12:02 +0100

    Serve page from public folder

--- src/Server/server.js
+++ src/Server/server.js
@@ -5,11 +5,7 @@ import path from 'path';
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use((req, res, next) => {
