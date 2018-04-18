7807c612f71e28d21e556c9517c44f5c6040bcac
rocco <rocco.righi@bbc.co.uk>
2018-04-18 18:23:40 +0100

    Remove certs and unneeded https module

--- src/Server/index.js
+++ src/Server/index.js
@@ -1,17 +1,5 @@
 import server from './server';
-import fs from 'fs';
-import https from 'https';
-import path from 'path';
 
-const key = fs.readFileSync(path.resolve(__dirname + '/../crypto-key.pem'));
-const cert = fs.readFileSync(path.resolve(__dirname + '/../crypto-cert.pem'))
-
-const options = {
-    key,
-    cert
-};
-
-// const server = https.createServer(options, app);
 const PORT = 8080;
 server.listen(process.env.PORT || PORT, () => console.log('App listening at port %s', PORT));
 
